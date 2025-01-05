23d5b0154e5c64fc62c4ab9412bf7b2964230948
khlick <khrisgriffis@gmail.com>
2025-01-04 23:02:55 -0800

    Fixed template white-space after hidden code blocks

--- labpaper/__init__.py
+++ labpaper/__init__.py
@@ -1,6 +1,6 @@
 """LabPaper - A Jupyter notebook exporter for academic papers."""
 
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 
 from .exporters import SpringerNaturePDF
 from .preprocessors import PythonMarkdownPreprocessor, PygmentizePreprocessor
